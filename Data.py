2d314697299ad37bfe6fee0157251a742f738bb4
fmoralesdiaz <65556328+fmoralesdiaz@users.noreply.github.com>
2022-03-08 18:09:18 -0600

    the final thing

--- Data.py
+++ Data.py
@@ -11,15 +11,16 @@ class Data:
     lat_file = open("france-latlong-sav.txt")
 
     lat_long_dict={}
+    test ={}
 
     for input_line in lat_file:
      input_line = input_line.strip()
     
-    #using regex to group ouput and use matcher object 
-     regex_pattern = re.compile("(^.+\w),.+,\s.+\s(-?\d+\.\d+)\s(-?\d+\.\d+)")
+     #using regex to group ouput and use matcher object 
+     regex_pattern = re.compile("(^.+\w),.+,\s.+\s(\-?\d+\.\d+)\s(\-?\d+\.\d+)")
      matchedOutput = regex_pattern.match(input_line)
 
-    #if there is a line that doesnt match the pattern print an error message
+     #if there is a line that doesnt match the pattern print an error message
      if not matchedOutput:
         #print("incorrect input")
         continue
@@ -30,5 +31,7 @@ class Data:
 
      lat_long_dict['lat']= float(city_lat)
      lat_long_dict['long']=float(city_long)
-     self.db[city_name]=lat_long_dict
+     #self.db[city_name]=lat_long_dict
+     test[city_name] = lat_long_dict
+    self.db = test
     lat_file.close()
